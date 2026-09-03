c9e1aa605efd94c75d9a48e76faa1ad1ac161034
aslisudesahin <github-aslisahin357@gmail.com>
2026-09-03 13:43:07 +0300

    Update backend and Docker configuration

--- backend/models/tasks.js
+++ backend/models/tasks.js
@@ -4,9 +4,9 @@ const taskSchema = new mongoose.Schema({
     text: { type: String, required: true },
     completed: { type: Boolean, default: false },
     date: { type: String },
-    category: { type: String, default: 'Work' }, // Kategori alanı
-    priority: { type: String, default: 'medium' }, // Öncelik alanı
-    userId: { type: mongoose.Schema.Types.Mixed, required: true } // Kullanıcı ID eşleşmesi için
+    category: { type: String, default: 'Work' }, 
+    priority: { type: String, default: 'medium' }, 
+    userId: { type: mongoose.Schema.Types.Mixed, required: true }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
